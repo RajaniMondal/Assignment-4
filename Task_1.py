1a2506357dc6da401eced3592eeab20324c83006
RajaniMondal <102131572+RajaniMondal@users.noreply.github.com>
2025-08-10 12:23:26 +0530

    Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -3,10 +3,20 @@ file1 =open('sample.txt', 'r')
 readingfile= file1.read ()
 print(readingfile)
 
-# Correcting the error and completing the task
+#Correcting the error and completing the task
 file1 =open('sample.txt', 'r')
 readingline_1= file1.read (35)
 readingline_2= file1.read (34)
 print('Reading file contents:')
 print( readingline_1)
 print(readingline_2)
+file1.close()
+
+#Alternative method
+try:
+    file1 =open('sample.txt', 'r')
+    print('Reading file contents:')
+    for line in file1:
+            print(line.strip())  # strip() removes extra newline characters
+except FileNotFoundError:
+    print("Error: 'sample.txt' does not exist. Please check the file name or create the file.")
